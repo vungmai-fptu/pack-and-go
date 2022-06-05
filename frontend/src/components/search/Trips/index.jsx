8fdbff359a972711166c5375891142adf95ce3ce
lnson <leson131002@gmail.com>
2022-06-05 15:05:12 +0700

    delete .ide

--- frontend/src/components/search/Trips/index.jsx
+++ frontend/src/components/search/Trips/index.jsx
@@ -6,14 +6,14 @@ import styles from './Trips.module.css';
 const Trips = () => {
   return (
     <div>
-        <div className={styles["trips-title"]}>
-          <div className={styles["title-wrapper"]}><label className={styles.title}>Trips</label>
+      <div className={styles["trips-title"]}>
+        <div className={styles["title-wrapper"]}><label className={styles.title}>Trips</label>
           <button className={styles["add-btn"]}><div className={styles["add-btn-title"]}><span className={["add-btn-text"]}>Show all</span></div></button>
-          </div>
         </div>
-        <div className={styles["trip-container"]}>
-          <TripCardSlider/>
-        </div>
+      </div>
+      <div className={styles["trip-container"]}>
+        <TripCardSlider />
+      </div>
     </div>
   )
 }
