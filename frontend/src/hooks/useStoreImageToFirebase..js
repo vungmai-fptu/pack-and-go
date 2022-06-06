2fa3a017248ea19fd44808b45f0f25260c890d81
lnson <leson131002@gmail.com>
2022-06-06 20:18:36 +0700

    Add store image to firebase

---
+++ frontend/src/hooks/useStoreImageToFirebase..js
@@ -0,0 +1,8 @@
+import { useState } from "react"
+
+export const useStoreImageToFireBase = () => {
+  const [isSuccess, SetIsSuccess] = useState(false);
+  const [error, setError] = useState(null);
+
+
+}
