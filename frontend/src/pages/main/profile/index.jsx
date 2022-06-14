9a16cfcdb9b0f1c40cdfeec488e6edea22164b3b
Vu Ngoc Mai <vungocmai322@gmail.com>
2022-06-14 22:20:41 +0700

    vtnmai/ update profile page ~ final

--- frontend/src/pages/main/profile/index.jsx
+++ frontend/src/pages/main/profile/index.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import MapComponent from "../../../components/map";
+//import MapComponent from "../../../components/map";
 import CountryList from "../../../components/profile/CountryList";
 import FutureTrips from "../../../components/profile/FutureTrips";
 import Header from "../../../components/profile/Header";
-import Map from "../../../components/profile/Map";
-// import Map from "../../../components/profile/Map";
-import MapNav from "../../../components/profile/MapNav";
+//import Map from "../../../components/profile/Map";
+import TabGroup from "../../../components/profile/MapNav/TabGroup";
 import Trips from "../../../components/profile/Trips";
 
 const Profile = () => {
   return (
     <>
       <Header />
-      <MapNav />
-      <Map/>
+      <TabGroup/>
       <FutureTrips />
       <Trips />
       <CountryList />
