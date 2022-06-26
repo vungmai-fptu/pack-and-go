3a1fd42372b3af6c0b385e8bc1d0dd2404948291
tranquoclong <tranquoclong1310@gmail.com>
2022-06-26 18:43:19 +0700

    ko

--- frontend/src/store/actions/trip.action.jsx
+++ frontend/src/store/actions/trip.action.jsx
@@ -49,18 +49,9 @@ export const saveTrip = (trip) => {
   };
 };
 
-<<<<<<< HEAD
-export const setTrip = (id) => {
-  return async (dispatch, getState) => {
-
-    const { user } = getState().user;
-=======
 export const setTrip = (id, setErrorTrip) => {
-  return async (dispatch) => {
-    const userLogin = localStorage.getItem("userLogin");
-    console.log(userLogin);
->>>>>>> 6a1ce2e5383c515a2a9d7aaf8e129ec1660a5c2a
-
+  return async (dispatch, getState) => {
+    const { user } = getState().user;
     dispatch(startLoading());
     await axios({
       method: "GET",
@@ -70,14 +61,10 @@ export const setTrip = (id, setErrorTrip) => {
         dispatch(stopLoading());
         const { data } = res;
 
-<<<<<<< HEAD
-        const canUpdate = user && (user.username === data.owner || data.tripMates.includes(user.username));
-=======
         const canUpdate =
-          userLogin &&
-          (JSON.parse(userLogin).username === data.owner ||
-            data.tripMates.includes(JSON.parse(userLogin).username));
->>>>>>> 6a1ce2e5383c515a2a9d7aaf8e129ec1660a5c2a
+          user &&
+          (user.username === data.owner ||
+            data.tripMates.includes(user.username));
 
         const visitDays = data.visitDays.map((day) => ({
           id: Math.random().toString().substring(2, 9),
