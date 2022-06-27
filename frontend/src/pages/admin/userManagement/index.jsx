2b7f2d66fdfbc3e160433bd55b9ba2fe8e801e88
tranquoclong <tranquoclong1310@gmail.com>
2022-06-27 12:01:42 +0700

    add dasboard

--- frontend/src/pages/admin/userManagement/index.jsx
+++ frontend/src/pages/admin/userManagement/index.jsx
@@ -1,7 +1,43 @@
 import React from "react";
-
+import styles from "../Dashboard/dashboard.module.css";
 function UserManagement() {
-  return <div>UserManagement</div>;
+  return (
+    <div className={styles.container}>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Types</th>
+            <th>Last updated at</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Developer Zahid</td>
+            <td>
+              <span className={styles.status_indicatorActive} />
+              Active
+            </td>
+            <td>Attendee, F1</td>
+            <td>Jul 10, 2023, 01:10 PM</td>
+            <td>Việt Nam</td>
+          </tr>
+          <tr>
+            <td>Developer Zahid</td>
+            <td>
+              <span className={styles.status_indicator} />
+              Active
+            </td>
+            <td>Attendee, F1</td>
+            <td>Jul 10, 2023, 01:10 PM</td>
+            <td>Việt Nam</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default UserManagement;
