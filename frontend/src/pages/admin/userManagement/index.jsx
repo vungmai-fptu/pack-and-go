ebc481abb658fda981e78372a762de6a41a4bef6
tranquoclong <tranquoclong1310@gmail.com>
2022-06-28 11:57:03 +0700

    update dashboard

--- frontend/src/pages/admin/userManagement/index.jsx
+++ frontend/src/pages/admin/userManagement/index.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getListUser } from "../../../store/actions/user.action";
 import styles from "../Dashboard/dashboard.module.css";
 function UserManagement() {
+  const dispatch = useDispatch();
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const [userList, setUserList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(
+    () => {
+      dispatch(
+        getListUser(page, userList, setUserList, setTotalPages, setLoading)
+      );
+    },
+    // eslint-disable-next-line
+    [page]
+  );
+
   return (
     <div className={styles.container}>
       <table>
@@ -8,32 +25,56 @@ function UserManagement() {
           <tr>
             <th>Name</th>
             <th>Status</th>
-            <th>Types</th>
-            <th>Last updated at</th>
+            <th>Total Trips</th>
             <th>Country</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Developer Zahid</td>
-            <td>
-              <span className={styles.status_indicatorActive} />
-              Active
-            </td>
-            <td>Attendee, F1</td>
-            <td>Jul 10, 2023, 01:10 PM</td>
-            <td>Việt Nam</td>
-          </tr>
-          <tr>
-            <td>Developer Zahid</td>
-            <td>
-              <span className={styles.status_indicator} />
-              Active
-            </td>
-            <td>Attendee, F1</td>
-            <td>Jul 10, 2023, 01:10 PM</td>
-            <td>Việt Nam</td>
-          </tr>
+          {userList.map((listUser, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <img
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      borderRadius: "50%",
+                      marginRight: "10px",
+                    }}
+                    alt="profile"
+                    src={
+                      listUser.profileImageUrl === ""
+                        ? "https://wrld-se-prod.b-cdn.net/images/user-empty.svg?width=640&height=640"
+                        : listUser.profileImageUrl
+                    }
+                  />
+                  <div>
+                    <div> {listUser.username}</div>
+                    <div> {listUser.username}</div>
+                  </div>
+                </td>
+                <td>
+                  <span className={styles.status_indicatorActive} />
+                  Active
+                </td>
+                <td>10</td>
+                <td>
+                  {listUser.country !== null && listUser.country !== ""
+                    ? listUser.country
+                    : "Việt Nam"}
+                </td>
+                <td>
+                  <div className={styles.save_button}>
+                    <button>Update</button>
+                  </div>
+                  <div className={styles.close_button}>
+                    <button>Delete</button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
